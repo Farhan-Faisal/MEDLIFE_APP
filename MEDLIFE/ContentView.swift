0d21ef533da915e32febc978d7c538e11fdb2b5a
Farhan-Faisal <farhan.faisal@mail.utoronto.ca>
2022-12-30 11:01:58 -0500

    Initial Commit

---
+++ MEDLIFE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MEDLIFE
+//
+//  Created by Farhan Bin Faisal on 2022-12-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MEDLIFE/MEDLIFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEDLIFEApp.swift
+//  MEDLIFE
+//
+//  Created by Farhan Bin Faisal on 2022-12-30.
+//
+
+import SwiftUI
+
+@main
+struct MEDLIFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
